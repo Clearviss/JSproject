a5a58c1f0b61b42a689652d3250944cc428aa757
DirtyVision <90791681+DirtyVision@users.noreply.github.com>
2023-09-15 13:23:47 +0200

    dodano polityke i regulamin

---
+++ main/script.js
@@ -0,0 +1,1 @@
+<p>Jeśli użytkownik nie chce otrzymywać plików cookies, może zmienić ustawienia przeglądarki. Zastrzegamy, że wyłączenie obsługi plików cookies niezbędnych dla procesów uwierzytelniania, bezpieczeństwa, utrzymania preferencji użytkownika może utrudnić, a w skrajnych przypadkach może uniemożliwić korzystanie ze stron www.</p>
